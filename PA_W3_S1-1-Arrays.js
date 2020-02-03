da8df2437633b83a864acfcb2a91ec77384e9566
Jordan Walker <jwalker117@gmail.com>
2020-02-03 15:12:45 -0600

    Adding W3 Video 1

---
+++ PA_W3_S1-1-Arrays.js
@@ -0,0 +1,27 @@
+/*
+	Arrays Conceptual
+	
+	Variable that can have multiple values
+
+	Empty array
+	let name =[];
+
+	Example of Array
+	let things = ['thing1', 'thing2', 'thing3'];
+
+	Arrays have indexes. They start at 0 and then go up.
+	thing1 index 0
+	thing2 index 1
+	thing3 index 2
+
+	Accessing an array
+	things[0]; returns thing1
+
+	Methods on Arrays
+	length(); shows you how many items are in the array
+	push(); adds a value to the array
+
+	things.length; will return 3
+	things.push('thing4'); adds thing4 to the end of the array
+
+*/
