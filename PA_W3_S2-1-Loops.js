3c3251998964e6d001511d19ec0a7bf72052906e
Jordan Walker <jwalker117@gmail.com>
2020-02-04 15:41:05 -0600

    Adding W3 Video 3

---
+++ PA_W3_S2-1-Loops.js
@@ -0,0 +1,49 @@
+/*
+	Loops Conceptual
+
+	- repeated code
+	- block of code
+	- conditional statements
+
+	Two types of loops
+	- for loops
+	- while loops
+
+	While condition is true 
+
+	for a# of items
+	for a# of times
+
+	WHILE LOOPS
+
+	while hungry, I'll keep eating
+		while(hungry) {
+			keep eating;
+		}
+
+	while(water <= capcityOfCup) {
+		water++; //keep filling up the cup with water
+	}
+
+	FOR LOOPS
+
+	for each piece of pizza in the pie {
+		eat pizza
+	}
+
+	for each card in the array, check to see if its a spade.
+	
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
