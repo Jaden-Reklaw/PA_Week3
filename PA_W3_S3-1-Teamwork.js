cfc6e73e4ce98bb4cc9df520db70cb16497f9cc4
Jordan Walker <jwalker117@gmail.com>
2020-02-05 19:56:49 -0600

    Adding W3 Video 5

---
+++ PA_W3_S3-1-Teamwork.js
@@ -0,0 +1,87 @@
+/*
+	Effective Questions and Research
+
+	1.) Search
+	2.) Store
+	3.) Share
+
+	SEARCH
+	Frame your question 3 differnt ways.
+	10 Tab rule. Fast scan.
+	Advanced Searches
+
+	STORE
+	Tag and label the links in useful bookmarks
+	
+	SHARE
+	Post on slack. Forums
+
+	Tips for Google search
+
+	1.) How to find related sites?
+	related: [website]
+	//This will finds sites like google and facebbok
+	related: google.com
+	related: facebook.com
+
+	2.) HOw to find filetypes through search
+	[what your searching]filetype: [extension]
+	//the following will find pdf and docs
+	mac os viruses filetype:pdf
+	mac os viruses filetype:doc
+
+	3.) Using Serach operators like + or -
+	[search] +[search]
+	//This will find Tom Cruise things that have scientology
+	Tom Cruise +Scientology
+	//This will omit any articles that have scientology
+	Tom Cruise -Scientology
+
+	4.) Using "" to find a specific phrase and *  to find variations
+	"quote or search item"
+	//The following will find things that spefically say may the force be with you
+	"may the force be with you"
+	//Put an * for force and it will return a lot of different results
+	"may the * be with you"
+
+	5.) Using Nearby in order to find places near you
+	[search] nearby
+	//Will find coffee shops nearby
+	coffee nearby
+
+	6.) Viewing cached sites that might be down
+	Search in google then click on the down v next to the site then click cached
+	This gives a static view of the site that google cached
+
+	7.) How to search for images for sizes
+	Right click on image then click search google for image
+	Then select all sizes, small or medium
+
+	8.) Searching specifically in a website for a specific thing
+	[search] site:[website]
+	//This will look for augmented reality in theverge.com
+	augmented reality site:theverge.com
+	//This will only give articles from schools
+	augmented reality site:.edu
+
+	9.) Search for information that is up to date
+	Search for some information then select tools then anytime dropdown
+	then select the time of choice. Custom time is good to select dates
+
+	10.) Stock Prices
+	Search the companies ticker symbol
+
+	11.) Search Terms in the title of the website
+	intitle: [search]
+	//This will give you all websites with titles that contain Houston
+	intitle: Houston
+	//Titles with three key terms
+	allintitle: [search]
+	allintitle: Houston hurricane relief
+
+	12.) Advanced Search
+	Click on setting then click advanced search
+	Fill out all the fields then enter to find results
+	Beware the more specific the less results or none at all
+
+*/
